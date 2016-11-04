2b17d2a3d77833ab59d0fd967dbb54be657f87ca
Justin Maher <justin.maher@citrix.com>
2016-11-04 15:48:49 -0700

    Added addOnce method and supporting unit tests

--- src/micro-signals.ts
+++ src/micro-signals.ts
@@ -4,12 +4,22 @@ export interface SignalBinding {
 
 export class Signal<T> {
     private _listeners = new Set<(payload: T) => void>();
+
     add(listener: (payload: T) => void): SignalBinding {
         this._listeners.add(listener);
         return {
             detach: () => this._listeners.delete(listener),
         };
     }
+
+    addOnce(listener: (payload: T) => void) {
+        const binding = this.add((payload: T) => {
+            binding.detach();
+            listener(payload);
+        });
+        return binding;
+    }
+
     dispatch(payload: T): void {
         this._listeners.forEach(callback => callback.call(undefined, payload));
     }
